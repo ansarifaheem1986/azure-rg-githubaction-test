e9473ce1ffe54a286e64930b95813722977ee2f4
Ansari Faheem <159180782+ansarifaheem1986@users.noreply.github.com>
2025-07-24 17:33:11 -0500

    Added the azurerm version1

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 #Terraform Settings block
 terraform {
   required_version = "1.11.3"
+
   required_providers {
+    azurerm = {
     source = "hashicorp/azurerm"
     version = ">= 4.25.0"
+   }
   }
 }
 
