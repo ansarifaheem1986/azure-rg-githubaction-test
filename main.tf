e13b1b7d621b454c190ddd1f5ef641beb8f5a055
Ansari Faheem <159180782+ansarifaheem1986@users.noreply.github.com>
2025-07-24 16:34:56 -0500

    Initial push to new repo

---
+++ main.tf
@@ -0,0 +1,15 @@
+#Terraform Settings block
+terraform {
+  required_version = "1.11.3"
+  required_providers {
+    source = "hashicorp/setup-terraform@v2"
+    #source = "hashicorp/azurerm"
+    version = "~> 3.0"
+  }
+}
+
+resource "azurerm_resource_group" "myrg" {
+  name = "DevOps-nonprod-eastus-01-rg"
+  location = "eastus"
+  #resource block to create the azure resource group
+}
